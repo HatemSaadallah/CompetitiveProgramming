c68e10f6ecf335e72806c38fce9a22cbd1ffe13d
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-08-21 00:12:46 +0300

    ClockSeconds not working

--- Codeforces/CodeRams Practice Problem Archive/Distance.cpp
+++ Codeforces/CodeRams Practice Problem Archive/Distance.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 int main(){
-    long long x1, x2, y1, y2;
+    double x1, x2, y1, y2;
 
     cin >> x1 >> y1 >> x2 >> y2;
-
-    cout << sqrtf(powf(x2-x1, 2) + powf(y2-y1, 2));
+    double h1 = pow(x2-x1, 2);
+    double h2 = pow(y2-y1, 2);
+    double sq = sqrt(h1 + h2);
+    cout << fixed;
+    cout << setprecision(16);
+    cout << sq;
 }
