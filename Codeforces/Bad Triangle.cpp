d241ba2c035695e9bfee25f232a21505b52189af
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-08-21 17:24:42 +0300

    Bad Triangle Solved

--- Codeforces/Bad Triangle.cpp
+++ Codeforces/Bad Triangle.cpp
@@ -1,19 +1,48 @@
 #include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++){
-        
-        int l;
-        cin >> l;
-        vector<int> array;
-        for(int k=0; k < l; k++){
-            int m;
-            cin >> m;
-            array.push_back(m);
-        }
+    #include <math.h>
+ 
+    using namespace std;
+    typedef long long ll;
+    typedef unsigned long long ull;
+    # define M_PI  3.14159265358979323846
+ 
+ 
+    const int M=1e9+7;
+    long long mod(long long x){
+        return ((x%M + M)%M);
     }
-    cout << "Done";
-}
+    long long add(long long a, long long b){
+        return mod(mod(a)+mod(b));
+    }
+ 
+    void solve(){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        for(int i=0;i+2<n;i++){
+            if(a[i]+a[i+1]<=a[n-1]){
+                cout<<i+1<<" "<<i+2<<" "<<n<<"\n";
+                return;
+            }
+        }
+        cout<<-1<<"\n";
+        return;
+    }
+    int main(){
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        cout<<fixed;
+        cout<<setprecision(10);
+//        freopen("timber_input.txt", "r", stdin);
+//        freopen("timber_output.txt", "w", stdout);
+        int t=1;
+        cin>>t;
+        for(int i=1;i<=t;i++){
+//            cout<<"Case #"<<i<<": ";
+            solve();
+        }
+        return 0;
+    }
