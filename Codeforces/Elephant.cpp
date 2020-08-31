cb9ad24b4b246f17f773f180cdda5466af1a52fd
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-08-31 23:30:46 +0300

    Elephant silly problem

---
+++ Codeforces/Elephant.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+// #include <bits/stdc++.h> 
+#include <math.h>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1000000007
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+int32_t main(){
+    fast
+	int n;
+	cin >> n;
+	int res = 0;
+	while(n>0){
+		res++;
+		n -= 5;
+	}
+	cout << res;
+
+	return 0;
+}
+
+
+
