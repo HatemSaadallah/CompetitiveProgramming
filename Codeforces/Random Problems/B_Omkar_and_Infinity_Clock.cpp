7582cace4ecb1062ef35afcb8ed52b31264574a3
HatemSaadallah <hatem200011@gmail.com>
2020-09-09 20:38:57 +0300

    Another failed try :(

---
+++ Codeforces/Random Problems/B_Omkar_and_Infinity_Clock.cpp
@@ -0,0 +1,80 @@
+// Create your own template by modifying this file!
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <sstream>
+#include <iterator>
+
+using namespace std;
+
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1000000007
+#define int long long
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+
+struct debugger
+{
+    template <typename T>
+    debugger &operator,(const T &v)
+    {
+        cerr << v << " ";
+        return *this;
+    }
+} dbg;
+
+int getMax(int x[], int size)
+{
+    int max = 1e-9;
+    for (int i = 0; i < size; i++)
+    {
+        if (x[i] > max)
+            max = x[i];
+    }
+    return max;
+}
+
+int32_t main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int len, op;
+        cin >> len >> op;
+        int x[len];
+        int max = 1e-9;
+        for (int i = 0; i < len; i++)
+        {
+            int n;
+            cin >> n;
+            x[i] = n;
+            if (n > max)
+            {
+                max = n;
+            }
+        }
+        for(int l=0;l<op;l++){
+            for(int m=0;m<len;m++){
+                x[m]=getMax(x, len)-x[m];
+            }
+        }
+        for(int num: x){
+            cout << num << " ";
+        }
+    }
+}
