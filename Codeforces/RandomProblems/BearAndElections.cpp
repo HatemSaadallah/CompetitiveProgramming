99837a4134bcc09b9e3ed647ff110068e31c751b
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-11-12 23:41:38 +0200

    Bear and Elections

---
+++ Codeforces/RandomProblems/BearAndElections.cpp
@@ -0,0 +1,79 @@
+/*input
+
+*/
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <math.h>
+#include <sstream>
+#include <iterator>
+#include <cstdlib>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+#define endl ("\n")
+#define pi (3.141592653589)
+#define mod 1000000007
+#define int int64_t
+#define float double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(c) c.begin(), c.end()
+#define min3(a, b, c) min(c, min(a, b))
+#define min4(a, b, c, d) min(d, min(c, min(a, b)))
+#define rrep(i, n) for(int i=n-1;i>=0;i--)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+
+struct debugger
+{
+    template<typename T> debugger& operator , (const T& v)
+    {    
+        cerr<<v<<" ";    
+        return *this;    
+    }
+} dbg;
+
+bool check_key(map<int, int> m, int key) 
+{ 
+    if (m.find(key) == m.end()) 
+        return false; 
+  
+    return true; 
+} 
+
+
+int32_t main() 
+{
+    fast;
+	int t;
+	cin >> t;
+	vector<int> x;
+  int curr;
+  rep(i, t){
+    int n;
+    cin >> n;
+    if(i==0){
+        curr = n;
+    } else{
+        x.pb(n);
+    }
+	}
+  sort(x.begin(), x.end(), greater<int>());
+  int res = 0;
+  while(curr <= x[0]){
+    x[0]--;
+    curr++;
+    res++;
+    sort(x.begin(), x.end(), greater<int>());
+  }
+  cout << res;
+  return 0;
+}
