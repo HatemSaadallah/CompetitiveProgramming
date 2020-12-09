cef00fa315320dd13742640ad3d5cfe19ead6133
Hatem Saadallah <saadallah.r.hatem@gmail.com>
2020-12-10 01:16:27 +0200

    autocommit on change

--- Codeforces/RandomProblems/HittheLottery.cpp
+++ Codeforces/RandomProblems/HittheLottery.cpp
@@ -84,20 +84,20 @@ int32_t main()
 	int res = 0;
 	while(n>0){
 		if(n>=100){
-		res += n/100;
-		n -= n%100;
+		res += n%100;
+		n -= n/100;
 		}
 		else if(n>=20){
-			res += n/20;
-			n -= n%20;
+			res += n%20;
+			n -= n/20;
 		}
 		else if(n>=10){
-			res += n/10;
-			n -= n%10;
+			res += n%10;
+			n -= n/10;
 		}
-		else if(n>5){
-			res += n/5;
-			n -= n%5;
+		else if(n>=5){
+			res += n%5;
+			n -= n/5;
 		}
 		else if(n>=1){
 			res += n;
